e8fd2faedf51bfd3d245b1de423fda94c0effc98
Mauri <maurig@generationstudents.org>
2025-08-29 14:27:30 -0300

    adiciona pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home/home'
+// import Home from './pages/home/Home'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
 
   return (
     <>
-      <Home/>
-    </>
+ <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              {/* <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} /> */}
+              <Route path="/" element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>    </>
   )
 }
 
